1c97271508f38e00f31c6854ca27e9cb62195ad5
Sumit Kumar <sumitbth67@gmail.com>
2024-08-24 00:17:52 +0530

    fixed bug

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const PORT = 9080
+const  app = express()
+
+app.get("/",(req,res)=>{
+    res.send("bol bhai  dd ")
+})
+
+
+app.listen(PORT,()=>{
+    console.log("server is running on ", PORT)
+})
+
+// app.listen(PORT,()=>{ 
+//     try {
+//         console.log(`Server is running on PORT :${PORT}`);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// })   
